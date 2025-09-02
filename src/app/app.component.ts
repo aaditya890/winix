f96b3503bbf0df2e45745006f977565f00111885
aaditya890 <mishraaaditya297@gmail.com>
2025-09-02 17:50:18 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,22 @@ import { filter } from "rxjs";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent{
-   constructor(private router: Router) {
+
+export class AppComponent {
+  constructor(private router: Router) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
       });
   }
+
+  // Function to open WhatsApp chat
+  openWhatsApp(): void {
+    const message = encodeURIComponent("Hii Winixair!");
+    const phone = "+918885241706";
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phone}&text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  }
+
 }
